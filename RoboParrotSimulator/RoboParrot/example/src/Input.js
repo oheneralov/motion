d437406e3f8acb257124c0199255296e2a593036
bgener <bgener@gmail.com>
2016-07-03 11:29:43 +0000

    initial draft for simulator page using reactjs

---
+++ RoboParrotSimulator/RoboParrot/example/src/Input.js
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Input = React.createClass({
+  getInitialState: function() {
+    return {value: 'goForwardOneStep();'};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+  onSubmit: function(event) {
+    	alert('Form submitted.' + this.state.value);
+      event.preventDefault();
+    },
+  render: function () {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <textarea className='form-control' onChange={this.handleChange}>
+          {this.state.value}
+          </textarea>
+          <p>{this.state.value}</p>
+          <button className='btn btn-success' type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Input/>, document.getElementById('app'));
