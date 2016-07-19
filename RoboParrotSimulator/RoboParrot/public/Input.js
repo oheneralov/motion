4db966a6b55ab32c9be8960876bba81933e55ff6
oheneralov <oheneralov@gmail.com>
2016-07-19 16:59:00 +0300

    added wings for the parrot

--- RoboParrotSimulator/RoboParrot/public/Input.js
+++ RoboParrotSimulator/RoboParrot/public/Input.js
@@ -1,39 +1,90 @@
 class Parrot {
     constructor(height, width) {
-        this.height = height;
-        this.width = width;
-  }
-	doStepForward() {
-		console.log("doing step forward");
-		//https://www.dashingd3js.com/svg-basic-shapes-and-d3js
+     console.log("creating the parrot...");
+	 //https://www.dashingd3js.com/svg-basic-shapes-and-d3js
 	 var svgContainer = d3.select("svg");
-	 var circle = svgContainer.append("circle")
+	 var head = svgContainer.append("circle")
 	                     .attr("cx", function() { return 50; })
-						 .attr("cy", function() { return 35; })
+						 .attr("cy", function() { return 27; })
 						 .attr("r", function() { return 25; })
 						 .attr("stroke-width", 2)
-                         .attr("stroke", "black");
+                         .attr("stroke", "black")
+						 .attr("fill", "none");
+						 console.log(head.attr("cy"));
+						 
+    var eye = svgContainer.append("circle")
+	                     .attr("cx", function() { return 53; })
+						 .attr("cy", function() { return 20; })
+						 .attr("r", function() { return 5; })
+						 .attr("stroke-width", 2)
+                         .attr("stroke", "black")
+						 .attr("fill", "none");
 						
-     var ellipse = svgContainer.append("ellipse")
+     var body = svgContainer.append("ellipse")
 	                     .attr("cx", function() { return 50; })
 						 .attr("cy", function() { return 100; })
 						 .attr("rx", function() { return 25; })
 						 .attr("ry", function() { return 50; })
+						 .attr("stroke-width", 2)
+                         .attr("stroke", "black")
+						 .attr("fill", "none");
+						 
+	var wing = svgContainer.append("ellipse")
+	                     .attr("cx", function() { return 50; })
+						 .attr("cy", function() { return 90; })
+						 .attr("rx", function() { return 20; })
+						 .attr("ry", function() { return 40; })
+						 .attr("stroke-width", 2)
+                         .attr("stroke", "black")
+						 .attr("fill", "none");					 
 	
-	svgContainer.append("line")
+	var leftLeg = svgContainer.append("line")
                          .attr("x1", 50)
                          .attr("y1", 150)
                          .attr("x2", 60)
                          .attr("y2", 250)
 						 .attr("stroke-width", 2)
-                         .attr("stroke", "black");
-	svgContainer.append("line")
+                         .attr("stroke", "black")
+						 .attr("fill", "none");
+						 
+						 
+	var rightLeg = svgContainer.append("line")
                          .attr("x1", 50)
                          .attr("y1", 150)
                          .attr("x2", 25)
                          .attr("y2", 250)
 						 .attr("stroke-width", 2)
-                         .attr("stroke", "black");
+                         .attr("stroke", "black")
+						 .attr("fill", "none");
+						 
+	//The data for our line
+    var lineData = [ { "x": 75,   "y": 25},  
+	                 { "x": 90,  "y": 30},
+                     { "x": 75,  "y": 35}, ];
+ 
+    //This is the accessor function
+    var lineFunction = d3.svg.line()
+                          .x(function(d) { return d.x; })
+                          .y(function(d) { return d.y; })
+                         .interpolate("linear");
+
+    //The line SVG Path we draw
+    var beak = svgContainer.append("path")
+                            .attr("d", lineFunction(lineData))
+                            .attr("stroke", "black")
+                            .attr("stroke-width", 2)
+                            .attr("fill", "none");
+						 
+						 
+    }
+	
+	
+	doStepForward() {
+	console.log("doing step forward");
+	//https://www.dashingd3js.com/svg-basic-shapes-and-d3js
+	 var svgContainer = d3.select("svg");
+	 
+
 	}
 }
 
